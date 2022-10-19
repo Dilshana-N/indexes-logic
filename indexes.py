43ce4994f0095da2ac8143e8e1fb13b42ea815ef
Dilshana-n <dilshanan2001@gmail.com>
2022-10-19 12:15:21 +0530

    find -ve & +ve indexes

---
+++ indexes.py
@@ -0,0 +1,8 @@
+n=int(input("Enter any number:"))
+if n>0:
+    print("The number {} is +ve".format(n))
+elif n==0:
+    print("The number {} is zero".format(n))
+else:
+    print("The number {} is -ve".format(n))
+
